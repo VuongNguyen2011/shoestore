9e512bd25ebf0446c9bb6bfa388cd7c04376145e
Trần Kim Phú <trankimphu0609@gmail.com>
2022-10-06 18:07:32 +0700

    index

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,29 +1,33 @@
 import React  from "react";
 import { Routes, Route } from "react-router-dom";
-import Product from "./pages/Product/Product";
-import Home from "./pages/Product/Home";
-import EditProduct from "./pages/Product/EditProduct";
+// import Product from "./pages/Product/Product";
+// import Home from "./pages/Product/Home";
+// import EditProduct from "./pages/Product/EditProduct";
+
+import Header from "./pages/Header/index";
 function App() {
   return (
-    <div>
-          <nav>
-            <ul>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/AddProduct">Product</a>
-              </li>
-            </ul>
-          </nav>
-    <Routes>
-    <Route path="/" element={<Home />}></Route>
-    <Route path="/AddProduct" element={<Product />}></Route>
-    <Route path="/edit-product/:id" element={<EditProduct />}></Route>
+    // <div>
+    //       <nav>
+    //         <ul>
+    //           <li>
+    //             <a href="/">Home</a>
+    //           </li>
+    //           <li>
+    //             <a href="/AddProduct">Product</a>
+    //           </li>
+    //         </ul>
+    //       </nav>
+    // <Routes>
+    // <Route path="/" element={<Home />}></Route>
+    // <Route path="/AddProduct" element={<Product />}></Route>
+    // <Route path="/edit-product/:id" element={<EditProduct />}></Route>
 
-    </Routes>
+    // </Routes>
 
-    </div>
+    // </div>
+
+    <Header />
     );
 }
 
