b8b51b2a20107ae6a026b5a30e4cde17c3599cfb
Nguyễn Ngọc Cảnh <nncanh2001@gmail.com>
2022-11-21 04:37:40 +0700

    Add login page

--- BE/app/Models/User.php
+++ BE/app/Models/User.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Tymon\JWTAuth\Contracts\JWTSubject;
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable 
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -52,7 +51,5 @@ class User extends Authenticatable implements JWTSubject
      *
      * @return array
      */
-    public function getJWTCustomClaims()
-    {
-        return [
+    
 }
